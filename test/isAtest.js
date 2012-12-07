7c8a8530ef183533662e037df41b59cb8895ec00
Oral Dalay <orald@vbi.vt.edu>
2012-12-06 23:46:01 -0500

    initial commit

--- test/isAtest.js
+++ test/isAtest.js
@@ -1,20 +1,47 @@
 "use strict";
 
 describe("Is A test!", function() {
-	var isNumericFalsy, isNumericTruthy;
+	var Falsy, Truthy;
+	
 	beforeEach(function() {
-		isNumericFalsy = ['D','XS','/','-',null,{},undefined,''];
-		isNumericTruthy = ['-1', '0', '0.5', '0.000000001', '100000000'];
-		
+
 	});
 	
-	it("isNumeric", function() {
-		for(var i in isNumericFalsy){
-			expect(SH.isNumeric(isNumericFalsy[i])).toBe(false);
-		}
-		for(var j in isNumericTruthy){
-			expect(SH.isNumeric(isNumericTruthy[j])).toBe(true);		
+	it("isNumericFalsy", function() {
+		Falsy = ['D','XS','/','-',{},undefined,''];
+		
+		for(var i in Falsy){
+			expect(SH.isNumeric(Falsy[i])).toBe(false);
 		}
 	});
+	
+	it("isNumericTruthy", function() {
+		Truthy = ['-1', '0', '0.5', '0.000000001', '100000000'];
+				
+		for(var j in Truthy){
+			expect(SH.isNumeric(Truthy[j])).toBe(true);		
+		}
+	});
+	
+	it("isPrimitiveFalsy", function() {
+		var n = new String(),
+			r = new RegExp(),
+			d = new Date(),
+			b = new Boolean();
+			
+		Falsy = [{}, [], n, r, d, b];		
+		for(var i in Falsy){
+			expect(SH.isPrimitive(Falsy[i])).toBe(false);
+		}
+	});
+	
+	it("isPrimitiveTruthy", function() {
+		Truthy = [undefined, false, true, 0, '1', -1, '', null];
+		
+		for(var j in Truthy){
+			expect(SH.isPrimitive(Truthy[j])).toBe(true);		
+		}
+	});
+	
 });
 
